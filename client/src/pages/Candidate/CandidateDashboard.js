40b141c561bbf577eb2a707b4e7a005fbc2f8582
Kirtan4544 <kbchaklasiya@gmail.com>
2024-06-19 19:43:42 -0400

    Created candidate dashboard design

--- client/src/pages/Candidate/CandidateDashboard.js
+++ client/src/pages/Candidate/CandidateDashboard.js
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from "react";
+import "../../styles/candidatedashboard.css";
+import CandidateHeader from "../../components/CandidateHeader";
 
 const CandidateDashboard = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <CandidateHeader />
+      <section className="container candidateDashboard">
+        <h2 className="title textPrimary">Dashboard</h2>
+        <div className="candidateDbCard rounded-6">
+          <div className="candidateDbGrid">
+            <div className="candidateDbProfile dbCardOne rounded-6 dFlex justifyCenter alignCenter">
+              <div className="candidateDbIcon bgOne dFlex alignCenter justifyCenter">
+                <i class="fa-solid fa-user textPrimary"></i>
+              </div>
+              <h3 className="textPrimary">Candidate Name</h3>
+              <p>
+                <i class="fa-solid fa-envelope textPrimary"></i>
+                <span className="textPlaceHolder">candiate@gmail.com</span>
+              </p>
+            </div>
+            <div className="candidateDbProfile dbCardTwo rounded-6 dFlex justifyCenter alignCenter">
+              <div className="candidateDbIcon bgTwo dFlex alignCenter justifyCenter">
+                <i class="fa-solid fa-briefcase textPrimary"></i>
+              </div>
+              <h3 className="textPrimary">Applied Jobs</h3>
+              <p className="textPlaceHolder fwBold fs20">1</p>
+            </div>
+            <div className="candidateDbProfile dbCardThree rounded-6 dFlex justifyCenter alignCenter">
+              <div className="candidateDbIcon bgThree dFlex alignCenter justifyCenter">
+                <i class="fa-solid fa-bookmark textPrimary"></i>
+              </div>
+              <h3 className="textPrimary">Saved Jobs</h3>
+              <p className="textPlaceHolder fwBold fs20">2</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default CandidateDashboard
+export default CandidateDashboard;

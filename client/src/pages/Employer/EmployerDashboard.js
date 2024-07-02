8d0449a51850359fa5df8a649d23be02b09ce5e5
Kirtan4544 <kbchaklasiya@gmail.com>
2024-07-01 20:06:14 -0400

    I have created employer dashboard design

--- client/src/pages/Employer/EmployerDashboard.js
+++ client/src/pages/Employer/EmployerDashboard.js
@@ -1,12 +1,44 @@
 import React from "react";
 import EmployerHeader from "../../components/EmployerHeader";
+import "../../styles/candidatedashboard.css";
 
 const EmployerDashboard = () => {
   return (
     <>
-    <EmployerHeader />
-  </>
-  ) 
+      <EmployerHeader />
+      <section className="container candidateDashboard">
+        <h2 className="title textPrimary">Dashboard</h2>
+        <div className="candidateDbCard rounded-6">
+          <div className="candidateDbGrid">
+            <div className="candidateDbProfile dbCardOne rounded-6 dFlex justifyCenter alignCenter">
+              <div className="candidateDbIcon bgOne dFlex alignCenter justifyCenter">
+                <i class="fa-solid fa-user textPrimary"></i>
+              </div>
+              <h3 className="textPrimary">Employer Name</h3>
+              <p>
+                <i class="fa-solid fa-envelope textPrimary"></i>
+                <span className="textPlaceHolder">candiate@gmail.com</span>
+              </p>
+            </div>
+            <div className="candidateDbProfile dbCardTwo rounded-6 dFlex justifyCenter alignCenter">
+              <div className="candidateDbIcon bgTwo dFlex alignCenter justifyCenter">
+                <i class="fa-solid fa-briefcase textPrimary"></i>
+              </div>
+              <h3 className="textPrimary">Total Jobs</h3>
+              <p className="textPlaceHolder fwBold fs20">1</p>
+            </div>
+            <div className="candidateDbProfile dbCardThree rounded-6 dFlex justifyCenter alignCenter">
+              <div className="candidateDbIcon bgThree dFlex alignCenter justifyCenter">
+                <i class="fa-solid fa-bookmark textPrimary"></i>
+              </div>
+              <h3 className="textPrimary">Number of Candidates</h3>
+              <p className="textPlaceHolder fwBold fs20">2</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default EmployerDashboard;

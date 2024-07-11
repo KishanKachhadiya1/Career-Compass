8378d3ed3a09383a9da7371976acb266d1765c5d
Kirtan4544 <kbchaklasiya@gmail.com>
2024-07-10 22:06:53 -0400

    Main website header responsive in all the device along with toggle menu which open sidebar at the right side and also make home page responsive for all the devices

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import logo from "../images/logo.png"
+import logo from "../images/logo.png";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header className="bgDarkGrey">
       <div className="container">
@@ -10,36 +16,45 @@ const Header = () => {
           <div className="logo dFlex alignCenter">
             <img src={logo} alt="Career Compass" />
           </div>
-          <ul className="dFlex menu-group">
-            <li className="menu-list">
-              <Link to="/" className="link">
-                Home
-              </Link>
-            </li>
-            <li className="menu-list">
-              <Link to="/jobs" className="link">
-                Jobs
-              </Link>
-            </li>
-            <li className="menu-list">
-              <Link to="/companies" className="link">
-                Companies
-              </Link>
-            </li>
-            <li className="menu-list">
-              <Link to="/about-us" className="link">
-                About Us
-              </Link>
-            </li>
-            <li className="menu-list">
-              <Link to="/contact-us" className="link">
-                Contact Us
-              </Link>
-            </li>
-          </ul>
-          <Link to="/login" className="btn">
-            Login
-          </Link>
+          <div className={`menu ${isMenuOpen ? "open" : ""}`}>
+            <ul className="dFlex menu-group">
+              <li className="menu-list">
+                <Link to="/" className="link">
+                  Home
+                </Link>
+              </li>
+              <li className="menu-list">
+                <Link to="/jobs" className="link">
+                  Jobs
+                </Link>
+              </li>
+              <li className="menu-list">
+                <Link to="/companies" className="link">
+                  Companies
+                </Link>
+              </li>
+              <li className="menu-list">
+                <Link to="/about-us" className="link">
+                  About Us
+                </Link>
+              </li>
+              <li className="menu-list">
+                <Link to="/contact-us" className="link">
+                  Contact Us
+                </Link>
+              </li>
+            </ul>
+            <Link to="/login" className="btn">
+              Login
+            </Link>
+          </div>
+          <button className="menu-toggle" onClick={toggleMenu}>
+            {isMenuOpen ? (
+              <i className="fa-solid fa-xmark"></i>
+            ) : (
+              <i className="fa-solid fa-bars"></i>
+            )}
+          </button>
         </nav>
       </div>
     </header>
